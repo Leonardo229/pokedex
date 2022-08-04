8ae502e1d1dc4326cf5d85574a34dba503afd2e1
Leonardo229 <leonardocelho@hotmail.com>
2022-08-04 16:21:41 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Leonardo Coelho on 04/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
